b345e908cb24410d4c727f5fbafadda79ce7fe8d
Daniel Zhang <traceless0714@gmail.com>
2026-06-15 21:22:54 -0500

    done fetching api using axios on homepage and passing products into gallery, gallery is mapped as well

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,34 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Gallery from '../components/Gallery';
+
 function Home() {
+    const [products, setProducts] = useState([]);
+    const [query, setQuery] = useState('');
+
+    const url = 'https://fakestoreapi.com/products';
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get(url);
+                console.log(response.data);
+                setProducts(response.data);
+            } catch (error) {
+                console.log(error.message);
+            }
+        };
+
+        fetchProducts();
+    }, []);
+
+    const selectproducts = products.filter(product =>
+        product.title.toLowerCase().includes(query.toLowerCase())
+    );
     return (
-        <></>
+        <>
+         <Gallery products={selectproducts} />
+        </>
     );
 }
 
