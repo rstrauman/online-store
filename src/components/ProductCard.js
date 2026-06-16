38bba1242524e6ae444ddfe7c03d9be720b6061f
Daniel Zhang <traceless0714@gmail.com>
2026-06-15 21:15:03 -0500

    productcard created, it has two buttons, one go into single product page, one button with onlick to add cart(incomplete)

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,6 +1,19 @@
-function ProductCard() {
+import { Link } from 'react-router-dom';
+
+function ProductCard(props) {
     return (
-        <></>
+        <div className='productcard'>
+            <div className='imgholder'>
+                <img src={props.image} alt={props.title} />
+            </div>
+            <div className='infoholder'>
+                <h2>{props.title}</h2>
+                <p>{props.category}</p>
+                <p>${props.price}</p>
+                <button><Link to={`/product/${props.id}`}>💡</Link></button>
+                <button onClick="">🛒</button>
+            </div>
+        </div>
     );
 }
 
